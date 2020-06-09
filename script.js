8685c80c5afa923b6b2aad59ae9fc9de98950051
odiri teddie adamu <odiriteddie.adamu@gmail.com>
2020-06-09 11:44:06 -0500

    marketing agency website sample

---
+++ script.js
@@ -0,0 +1,12 @@
+const closeButton = document.querySelector('.close-nav');
+const openButton = document.querySelector('.open-nav');
+const nav = document.querySelector('.nav');
+
+closeButton.addEventListener("click", () => {
+    nav.classList.remove('navigation-open');
+});
+
+openButton.addEventListener("click", () => {
+    nav.classList.add('navigation-open');
+});
+
